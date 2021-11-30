491b650c76e9092a908c05f2e55cdbebc9a088a8
Sapphire-code <85494077+Sapphire-code@users.noreply.github.com>
2021-11-30 12:27:49 +0800

    Update Maintain.js

--- Maintain.js
+++ Maintain.js
@@ -1,2 +1,4 @@
+// this file is for when i put the website under maintainance to make any updates to the website
+
 document.title = "Under Maintainance";
 document.body.InnerHTML = "Website Under Maintainance";
